8f08280c6e85393602eabcf554bb6513e9fe8092
YakhyojonKholmatov <yakhprogramming@gmail.com>
2023-01-05 22:29:05 +0500

    v1

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+       String strT=t+"";
+       int total=0,temp;
+        for (int i = 0; i < strT.length(); i++) {
+            temp = t%10;
+            t = (t-temp)/10;
+            total+=temp;
+        }
+        System.out.println(total);
     }
 }
+
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
+        int total=0,temp=0;
+        for (int i = 1; i < lengthOfLastNumber; i++) {
+            temp=temp+9;
+            total+=temp;
+           temp=temp*10;
 
+        }
+        System.out.println(temp);
     }
 }
